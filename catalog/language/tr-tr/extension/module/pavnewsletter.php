7961985ec031a25905d27389d6aa9e5d3a48c504
nonprofiterol <ilkerdemir@yandex.com.tr>
2017-12-24 23:41:51 +0200

    ikinciddddd commit

---
+++ catalog/language/tr-tr/extension/module/pavnewsletter.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$_['entry_sign_up_for_newsletter'] = "E-Mail bültenimize abone olun!";
+
+$_['entry_newsletter'] = "E-Mail Bülteni";
+
+$_['button_ok'] = "Tamam";
+
+$_['button_subscribe'] = "Kayıt Ol!";
+
+$_['default_input_text'] = "E-Mail adresiniz";
+
+$_['valid_email'] = "E-Mail adresi geçerli değil!";
+
+$_['success_post'] = "E-Mail bültenimize aboneliğiniz gerçekleştirildi.";
+
+$_['error_post'] = "E-Mail adresiniz zaten kayıtlı.";
+
+$_['description'] = "E-Mail bültenimiz, sizleri ilgiliendiren konularla ilgilidir ve düzenli olarak kayıtlı E-Mail adresinize gönderilir.";
+
+$_['description_fs'] = "Yeni eklenen ürünlerden, özel kampanya ve indirimlerden ve diğer önemli gelişmelerden önce siz haberdar olun.";
+
+?>
